7b919211f9ba74846b85e1ecfbfe6a37f9826536
Pedro Goulart Branco <72553813+PedroGoulartbranco@users.noreply.github.com>
2026-03-14 18:25:52 -0300

    testando o env

--- logica_filtros.py
+++ logica_filtros.py
@@ -1,5 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+chave_api = os.getenv("CHAVE_API")
+print(chave_api)
+
 filtros = ['poltrona', 'piscina', 'sofa', 'cama', 'cama casal', 'cama solteiro', 'traveseiro',
            'colchao']
 
 def tranformar_pesquisa_em_filtro(pesquisa):
-    
+    pass

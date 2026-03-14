4e17caac06d5df47a3962e3e494f18c34860fc89
Pedro Goulart Branco <72553813+PedroGoulartbranco@users.noreply.github.com>
2026-03-14 14:06:17 -0300

    mandando o json pro front

--- views/routes.py
+++ views/routes.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
+import json
 
 views_bp = Blueprint("views", __name__)
 
 @views_bp.route("/")
 def home():
     filtros = ['Nenhum']
-    return render_template("catalogo.html", filtros=filtros)
+    with open("data/produtos.json", encoding="utf-8") as pro:
+        produtos = json.load(pro)
+    return render_template("catalogo.html", filtros=filtros, produtos=produtos)

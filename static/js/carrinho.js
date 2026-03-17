7938ee293e298f051d7b21de84baac73f05c9d34
Pedro Goulart Branco <72553813+PedroGoulartbranco@users.noreply.github.com>
2026-03-17 14:54:43 -0300

    colocando descrição pro front

--- static/js/carrinho.js
+++ static/js/carrinho.js
@@ -1,8 +1,22 @@
 let produtos_no_carrinho = []
 let barra_lateral_adicionar_produto = document.getElementById("barra_lateral_adicionar_produto")
 let overlay = document.getElementById("overlay")
+let produto_atual = []
 
 function adicionar_no_carrinho(id_produto) {
+    produtos.forEach(produto => {
+        if (produto.id == id_produto) {
+            produto_atual = produto;
+        }
+    })
+    barra_lateral_adicionar_produto.innerHTML = `
+    <h2>Adicionar Produto</h2>
+    <p>${produto_atual.name}</p>
+    <img src="static/img/produtos/${produto_atual.img}" class="imagem" alt="${produto_atual.name}"  object-fit: contain;">
+    <p>Preço: R$${produto_atual.price}</p>
+    <p>${produto_atual.descricao_Front}</p>
+    `
+
     barra_lateral_adicionar_produto.classList.toggle("ativa")
     overlay.classList.toggle("ativa")
 }

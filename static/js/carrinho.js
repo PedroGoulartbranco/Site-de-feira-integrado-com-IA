8b146a3333e4f067710ed398635d561ac1db5daf
Pedro Goulart Branco <72553813+PedroGoulartbranco@users.noreply.github.com>
2026-03-17 14:31:21 -0300

    side bar de adiconar produto terminada

--- static/js/carrinho.js
+++ static/js/carrinho.js
@@ -1,6 +1,14 @@
 let produtos_no_carrinho = []
 let barra_lateral_adicionar_produto = document.getElementById("barra_lateral_adicionar_produto")
+let overlay = document.getElementById("overlay")
 
 function adicionar_no_carrinho(id_produto) {
     barra_lateral_adicionar_produto.classList.toggle("ativa")
+    overlay.classList.toggle("ativa")
+}
+
+function fechar_barras_laterais() {
+    console.log("fechou")
+    barra_lateral_adicionar_produto.classList.toggle("ativa")
+    overlay.classList.toggle("ativa")
 }

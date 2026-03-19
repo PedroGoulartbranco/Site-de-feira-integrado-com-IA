aba384fdc2ac39040105c0fb37601a9896bc09f5
Pedro Goulart Branco <72553813+PedroGoulartbranco@users.noreply.github.com>
2026-03-18 23:05:28 -0300

    testand 2

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from views.routes import views_bp
+import os
 
 app = Flask(__name__)
 
 app.register_blueprint(views_bp)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
